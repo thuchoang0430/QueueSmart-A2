0ae32e7aa3b4a2bd8ae48c20b531a79427b45d68
Huy Thuc Hoang <thuc.hhoang0430@gmail.com>
2026-07-04 11:59:38 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+function showScreen(id) {
+    // Hide all screens
+    const screens = document.querySelectorAll(".screen");
 
+    screens.forEach(screen => {
+        screen.classList.remove("active");
+    });
+
+    // Show selected screen
+    const selected = document.getElementById(id);
+
+    if (selected) {
+        selected.classList.add("active");
+    }
+}
+
+// Hide all screens when page loads
+window.onload = function () {
+    const screens = document.querySelectorAll(".screen");
+
+    screens.forEach(screen => {
+        screen.classList.remove("active");
+    });
+};
